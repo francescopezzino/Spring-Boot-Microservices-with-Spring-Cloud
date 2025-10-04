4cd87f9c931791d3ba1815516ee825cb7762f74f
Francesco Pezzino <francesco.pezzino@gmail.com>
2025-10-04 12:48:50 +0200

    currency exchange service

---
+++ currency-exchange-service/src/test/java/com/coursera/demomicroservices/currencyexchangeservice/CurrencyExchangeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coursera.demomicroservices.currencyexchangeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
